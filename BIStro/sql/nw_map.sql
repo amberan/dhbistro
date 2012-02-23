b030351d51941c7e6b88293762b8adabd8477650
amberan <amberan@gmail.com>
2012-02-23 18:24:19 +0000

    SQL pro mapy.

---
+++ BIStro/sql/nw_map.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
+--
+-- Počítač: localhost
+-- Vygenerováno: Čtv 23. úno 2012, 22:49
+-- Verze MySQL: 5.5.20
+-- Verze PHP: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databáze: `dhbistrocz`
+--
+
+--
+-- Vypisuji data pro tabulku `nw_map`
+--
+
+INSERT INTO `nw_map` (`id`, `datum`, `link`) VALUES
+(1, 0, 'http://maps.google.com/maps/ms?msa=0&msid=202010519094597531222.0004b849ff189c5e9f6a7'),
+(17, 1330035093, 'http://blog.peoplecomm.cz/clanek/13-cilu-cesko-slovenske-vychovy'),
+(18, 1330035117, 'http://maps.google.com/maps/ms?msa=0&msid=202010519094597531222.0004b849ff189c5e9f6a7'),
+(19, 1330035187, 'http://maps.google.com/maps/ms?msa=0&msid=202010519094597531222.0004b849ff189c5e9f6a7'),
+(20, 1330035200, 'http://maps.google.com/maps/ms?msa=0&msid=202010519094597531222.0004b849ff189c5e9f6a7');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
