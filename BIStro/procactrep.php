57b138750b8c529801c822564adb5c597db1bccc
amberan <amberan@gmail.com>
2012-02-28 06:05:14 +0000

    Vymazano zpracovani poznamek z procesnich souboru jednotlivych elementu - je sdruzeno v procnote.php

--- BIStro/procactrep.php
+++ BIStro/procactrep.php
@@ -83,14 +83,4 @@
 		}
 		Header ('Location: editactrep.php?rid='.$_GET['reportid']);
 	}
-	if (isset($_POST['setnote'])) {
-		if (!preg_match ('/^[[:blank:]]*$/i',$_POST['note']) && !preg_match ('/^[[:blank:]]*$/i',$_POST['title']) && is_numeric($_POST['secret'])) {
-			MySQL_Query ("INSERT INTO ".DB_PREFIX."notes VALUES('','".mysql_real_escape_string($_POST['note'])."','".mysql_real_escape_string($_POST['title'])."','".Time()."','".$usrinfo['id']."','3','".$_POST['caseid']."','".$_POST['secret']."')");
-		}
-		Header ('Location: '.$_POST['backurl']);
-	}
-	if (isset($_GET['deletenote'])) {
-		MySQl_Query("DELETE FROM ".DB_PREFIX."notes WHERE ".DB_PREFIX."notes.id=".$_GET['deletenote']);
-		Header ('Location: '.URLDecode($_GET['backurl']));
-	}
 ?>
