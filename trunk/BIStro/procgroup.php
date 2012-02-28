7a0aec9856e118cf641301992d4d9b5d7bf63979
amberan@gmail.com <amberan@gmail.com@23d67ad8-510b-267d-dae3-de19468680fb>
2012-02-28 06:05:14 +0000

    Vymazano zpracovani poznamek z procesnich souboru jednotlivych elementu - je sdruzeno v procnote.php

--- trunk/BIStro/procgroup.php
+++ trunk/BIStro/procgroup.php
@@ -69,14 +69,4 @@
 		}
 		Header ('Location: editgroup.php?rid='.$_GET['groupid']);
 	}
-	if (isset($_POST['setnote'])) {
-		if (!preg_match ('/^[[:blank:]]*$/i',$_POST['note']) && !preg_match ('/^[[:blank:]]*$/i',$_POST['title']) && is_numeric($_POST['secret'])) {
-			MySQL_Query ("INSERT INTO ".DB_PREFIX."notes VALUES('','".mysql_real_escape_string($_POST['note'])."','".mysql_real_escape_string($_POST['title'])."','".Time()."','".$usrinfo['id']."','2','".$_POST['groupid']."','".$_POST['secret']."')");
-		}
-		Header ('Location: '.$_POST['backurl']);
-	}
-	if (isset($_GET['deletenote'])) {
-		MySQl_Query("DELETE FROM ".DB_PREFIX."notes WHERE ".DB_PREFIX."notes.id=".$_GET['deletenote']);
-		Header ('Location: '.URLDecode($_GET['backurl']));
-	}
 ?>
