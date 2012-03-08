6dc65951be31ef086f4963626c26f208f7658e8a
amberan <amberan@gmail.com>
2012-03-08 17:27:34 +0000

    Zpetne provazby v breadcrumbu.

--- BIStro/addpersons.php
+++ BIStro/addpersons.php
@@ -8,7 +8,7 @@ if (isset($_POST['addtocase'])) {
 	}
 	pageStart ('Uložení změn');
 	mainMenu (5);
-	sparklets ('<a href="./cases.php">případy</a> &raquo; <a href="./editcase.php?rid='.$_POST['caseid'].'">úprava případu</a> &raquo; <strong>uložení změn</strong>');
+	sparklets ('<a href="./cases.php">případy</a> &raquo; <a href="./editcase.php?rid='.$_POST['caseid'].'">úprava případu</a> &raquo; <strong>uložení změn</strong>','<a href="readcase.php?rid='.$_POST['caseid'].'&hidenotes=0">zobrazit upravené</a>');
 	echo '<div id="obsah"><p>Osoby k případu uloženy.</p></div>';
 	if (isset($_POST['person'])) {
 			for ($i=0;$i<Count($person);$i++) {
@@ -25,7 +25,7 @@ if (isset($_POST['addtogroup'])) {
 	}
 	pageStart ('Uložení změn');
 	mainMenu (5);
-	sparklets ('<a href="./groups.php">skupiny</a> &raquo; <a href="./editgroup.php?rid='.$_POST['groupid'].'">úprava skupiny</a> &raquo; <strong>uložení změn</strong>');
+	sparklets ('<a href="./groups.php">skupiny</a> &raquo; <a href="./editgroup.php?rid='.$_POST['groupid'].'">úprava skupiny</a> &raquo; <strong>uložení změn</strong>','<a href="readgroup.php?rid='.$_POST['groupid'].'&hidenotes=0">zobrazit upravené</a>');
 	echo '<div id="obsah"><p>Osoby příslušné ke skupině uloženy.</p></div>';
 	if (isset($_POST['person'])) {
 		for ($i=0;$i<Count($person);$i++) {
@@ -57,7 +57,7 @@ if (isset($_POST['addtoareport'])) {
 	// header('Location: ./editactrep.php?rid='.$_POST['reportid']); // přesměrování zpět na předchozí stránku
 	pageStart ('Uložení změn');
 	mainMenu (5);
-	sparklets ('<a href="./reports.php">hlášení</a> &raquo; <a href="./editactrep.php?rid='.$_POST['reportid'].'">úprava hlášení</a> &raquo; <strong>uložení změn</strong>');
+	sparklets ('<a href="./reports.php">hlášení</a> &raquo; <a href="./editactrep.php?rid='.$_POST['reportid'].'">úprava hlášení</a> &raquo; <strong>uložení změn</strong>','<a href="readcase.php?rid='.$_POST['reportid'].'&hidenotes=0&truenames=0">zobrazit upravené</a>');
 	echo '<div id="obsah"><p>Osoby příslušné k hlášení uloženy.</p></div>';
 	pageEnd ();
 }
