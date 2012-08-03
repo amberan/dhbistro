6a7d4ad8612f2404dea63dc642a1e58b638f016d
amberan@gmail.com <amberan@gmail.com@23d67ad8-510b-267d-dae3-de19468680fb>
2012-08-03 11:21:46 +0000

    Ukryte heslo.

--- trunk/BIStro/settings.php
+++ trunk/BIStro/settings.php
@@ -3,12 +3,31 @@
 	pageStart ('Nastavení');
 	mainMenu (6);
 	sparklets ('<strong>nastavení</strong>');
+//	if (!isset ($hiddenpwd)) {
+//		$hiddenpwd='hidden';
+//	}
 ?>
-<div id="obsah"><strong>Nepoužívejte svá obvyklá hesla</strong>, protože v tomto systému se hesla ukládají nezakódovaná. <strong>Dají se v databázi přímo přečíst</strong>, takže si vymyslete něco čistě pro DI.<p></p></div>
-<form action="procsettings.php" method="post" id="inputform">
+<script type="text/javascript" language="JavaScript">
+<!--
+function pwdcheck(form)
+{
+	if (form.heslo.value != form.heslo2.value) {
+		alert ('Hesla nejsou stejná.');
+		return false
+	}
+	else return true;
+}
+-->
+</script>
+<div id="obsah"><strong>Nepoužívejte svá obvyklá hesla</strong>, protože v tomto systému se hesla ukládají nezakódovaná. <strong>Dají se v databázi přímo přečíst</strong>, takže si vymyslete něco čistě pro hru.<p></p></div>
+<form action="procsettings.php" method="post" name="inputform" id="inputform" onSubmit="return pwdcheck(this);">
 	<div>
 	<label for="heslo">Heslo:</label>
-  	<input type="text" name="heslo" id="heslo" value="<?php echo StripSlashes($usrinfo['pwd'])?>"/>
+  	<input type="password" name="heslo" id="heslo" value="<?php echo StripSlashes($usrinfo['pwd'])?>"/>
+	</div>
+	<div>
+	<label for="heslo2">Ověření:</label>
+  	<input type="password" name="heslo2" id="heslo2" value="<?php echo StripSlashes($usrinfo['pwd'])?>"/>
 	</div>
 	<div>
 	  <label for="plan">Aktuální plán:</label>
