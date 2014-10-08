708d5750c4d391a91df8dc80a4bbe506a182edf0
amberan <amberan@23d67ad8-510b-267d-dae3-de19468680fb>
2014-10-08 06:25:27 +0000

    Zmena deploymentoveho sql.

--- tags/1.1.0alpha/BIStro/procother.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once ('./inc/func_main.php');
-	if (isset($_REQUEST['delallnew'])) {
-	  MySQL_Query ("TRUNCATE ".DB_PREFIX."unread_".$usrinfo['id']);
-	  Header ('Location: '.$_REQUEST['delallnew']);
-	}
-?>
