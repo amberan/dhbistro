0c37c1b981a5a1d0fc7d887619eabb4f99855828
amberan <amberan@23d67ad8-510b-267d-dae3-de19468680fb>
2014-12-09 21:58:34 +0000

    Nova verze TinyMce

--- trunk/BIStro/js/tiny_mce_gz.js
+++
@@ -1,7 +0,0 @@
-tinyMCE_GZ.init({
-	plugins : "table,save,advhr,advimage,advlink,searchreplace,print,contextmenu,fullscreen",
-	themes : 'simple,advanced',
-	languages : 'cs',
-	disk_cache : false,
-	debug : false
-});
