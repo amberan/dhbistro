ae763b275f06ae9ffd752bb57d03cb860a7eae38
amberan <amberan@23d67ad8-510b-267d-dae3-de19468680fb>
2015-02-24 18:38:00 +0000

    Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek. Nepsat nazev databaze do sqlek.

--- BIStro/sql/20150208_nw_operation_type_mod.sql.sql
+++ BIStro/sql/20150208_nw_operation_type_mod.sql.sql
@@ -1,2 +1,2 @@
-INSERT INTO `dhbistrocz`.`nw_operation_type` (`id`, `name`) VALUES ('12', 'pokus o neoprávněný přístup');
-INSERT INTO `dhbistrocz`.`nw_operation_type` (`id`, `name`) VALUES ('13', 'pokus o přístup ke smazanému záznamu');
+INSERT INTO `nw_operation_type` (`id`, `name`) VALUES ('12', 'pokus o neoprávněný přístup');
+INSERT INTO `nw_operation_type` (`id`, `name`) VALUES ('13', 'pokus o přístup ke smazanému záznamu');
