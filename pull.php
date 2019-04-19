ce47fb859d4461f455df7b5e5c6cd4fad88cf6e5
Karel Křemel <charles@alembiq.net>
2019-04-19 09:37:05 +0200

    pull cleanup

--- pull.php
+++ pull.php
@@ -2,11 +2,8 @@
 require_once ($_SERVER['DOCUMENT_ROOT'].'/inc/func_main.php');
 use Tracy\Debugger;
 Debugger::enable(Debugger::PRODUCTION,$config['folder_logs']);
-$latte = new Latte\Engine;
-$latte->setTempDirectory($config['folder_cache']);
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
-
 if ( $_POST['payload'] ) {
 	Debugger::log('GIT WEBHOOK: '.$_POST['payload']);
 	$output = shell_exec( 'cd '.$_SERVER['DOCUMENT_ROOT'] .' && git reset --hard HEAD && git pull' );
