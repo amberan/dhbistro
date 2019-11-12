22369dfab8e689af367fbd8d5302648ee13d7824
Karel Křemel <charles@alembiq.net>
2019-11-12 09:18:59 +0100

    settings/menu text cleanup

--- custom/text.php
+++ custom/text.php
@@ -1,13 +1,27 @@
 <?php
-$text['point']='zlobod';
-$text['menu-zlobody']='Zlobody';
-$text['hlaseniV']='Hlášení';
-$text['hlaseniM']='hlášení';
 $text['jmeno']='Jméno';
 $text['heslo']='Heslo';
 $text['prihlasit']='Přihlásit';
 $text['bistro']='BIStro';
+$text['ulozit']='Uložit';
+$text['odhlaseniuspesne']='Byl jste úspěsně odhlášen.';
 
+// CHYBOVKY
+$text['zaznamnenalezen']='Požadovaný záznam nebyl nalezen!';
+$text['prazdnyvypis']='Výpis neobsahuje žádné položky!';
+$text['http401']='You are unauthorized to make this request!';
+
+// MENU
+$text['point']='zlobod';
+$text['menu-zlobody']='Zlobody';
+$text['hlaseniV']='Hlášení';
+$text['hlaseniM']='hlášení';
+$text['precistvse']='Přečíst vše';
+$text['opravduprecist']='Opravdu označit vše jako přečtené?';
+$text['hledat']='Hledat';
+$text['dalsi']='Další';
+
+// NASTAVENI
 $text['uzivatel']='Uživatel';
 $text['timeout']='Odhlásit po neaktivitě delší než';
 $text['vevterinach']='30-1800 vteřin';
@@ -15,13 +29,6 @@ $text['puvodniheslo']='Původní heslo';
 $text['noveheslo']='Nové heslo';
 $text['noveheslokontrola']='Nové heslo pro kontrolu';
 $text['aktualniplan']='Můj plán';
-
-$text['ulozit']='Uložit';
-
 $text['heslanesouhlasi']='Hesla nejsou stejná!';
-$text['zaznamnenalezen']='Požadovaný záznam nebyl nalezen!';
-$text['prazdnyvypis']='Výpis neobsahuje žádné položky!';
-$text['http401']='You are unauthorized to make this request!';
-$text['odhlaseniuspesne']='Byl jste úspěsně odhlášen.';
 
 ?>
