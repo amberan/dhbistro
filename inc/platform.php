f503a3bd51cbbff99a7a08a9d911a978f3b6bd1b
Karel Křemel <charles@alembiq.net>
2019-12-29 19:19:47 +0100

    platform bistro.alembiq.net

--- inc/platform.php
+++ inc/platform.php
@@ -1,67 +1,68 @@
 <?php
+
 switch ($_SERVER["SERVER_NAME"]) {
     case 'localhost':
-        $config['dbuser']=$config['dbdatabase']='dhbistrocz';
-		$config['custom']='DH';
-        $config['barva']='local';
+        $config['dbuser'] = $config['dbdatabase'] = 'dhbistrocz';
+        $config['custom'] = 'DH';
+        $config['barva'] = 'local';
         break;
     case 'www.dhbistro.cz':
-        $config['dbuser']=$config['dbdatabase']='dhbistrocz';
-        $config['barva']='dh';
-		$config['custom']='DH';
-		break;
+        $config['dbuser'] = $config['dbdatabase'] = 'dhbistrocz';
+        $config['barva'] = 'dh';
+        $config['custom'] = 'DH';
+        break;
     case 'nh.dhbistro.cz':
-        $config['dbuser']=$config['dbdatabase']='nhbistro';
-        $config['barva']='nh';
-		$config['custom']='NH';
+        $config['dbuser'] = $config['dbdatabase'] = 'nhbistro';
+        $config['barva'] = 'nh';
+        $config['custom'] = 'NH';
         break;
     case 'test.dhbistro.cz':
-        $config['dbuser']=$config['dbdatabase']='testbistro';
-        $config['barva']='test';
-		$config['custom']='DH';
+        $config['dbuser'] = $config['dbdatabase'] = 'testbistro';
+        $config['barva'] = 'test';
+        $config['custom'] = 'DH';
     break;
     case 'org.dhbistro.cz':
-        $config['dbuser']=$config['dbdatabase']='orgbistro';
-        $config['barva']='org';
-		$config['custom']='DH';
+        $config['dbuser'] = $config['dbdatabase'] = 'orgbistro';
+        $config['barva'] = 'org';
+        $config['custom'] = 'DH';
         break;
     case 'enigma.dhbistro.cz':
-        $config['dbuser']=$config['dbdatabase']='enigmabistro';
-		$config['barva']='enigma';
-		$config['custom']='enigma';
+        $config['dbuser'] = $config['dbdatabase'] = 'enigmabistro';
+        $config['barva'] = 'enigma';
+        $config['custom'] = 'enigma';
         break;
     case 'nhtest.dhbistro.cz':
-        $config['dbuser']=$config['dbdatabase']='nhtestbistro';
-		$config['barva']='test';
-		$config['custom']='NH';;
+        $config['dbuser'] = $config['dbdatabase'] = 'nhtestbistro';
+        $config['barva'] = 'test';
+        $config['custom'] = 'NH';;
         break;
     case 'bistro':
-        $config['dbuser']='bistro';
-        $config['dbdatabase']='bistro';
-        $config['barva']='local';
-		$config['custom']='NH';
-	    break;
+        $config['dbuser'] = 'bistro';
+        $config['dbdatabase'] = 'bistro';
+        $config['barva'] = 'local';
+        $config['custom'] = 'NH';
+        break;
     case 'bistro.alembiq.net':
-		$config['dbuser']='aliunde';
-		$config['dbdatabase']='test';
-		$config['barva']='test';
-		$config['custom']='NH';
-		break;
+        $config['dbuser'] = 'alembiq_bistro';
+        $config['dbdatabase'] = 'alembiq_bistro';
+        $config['barva'] = 'test';
+        $config['custom'] = 'NH';
+        break;
     case 'nhtestbistro.talmahera.eu':
-        $config['dbuser']=$config['dbdatabase']='nhtestbistro';
-        $config['barva']='test';
-		$config['custom']='NH';
-		break;
+        $config['dbuser'] = $config['dbdatabase'] = 'nhtestbistro';
+        $config['barva'] = 'test';
+        $config['custom'] = 'NH';
+        break;
     case 'dbp.talmahera.eu':
-        $config['dbuser']='dbp';
-	$config['dbdatabase']='dbpbistro';
-        $config['barva']='test';
-		$config['custom']='DB';
-		break;
+        $config['dbuser'] = 'dbp';
+    $config['dbdatabase'] = 'dbpbistro';
+        $config['barva'] = 'test';
+        $config['custom'] = 'DB';
+        break;
     case 'dhtestbistro.talmahera.eu':
-        $config['dbuser']=$config['dbdatabase']='dhtestbistro';
-        $config['barva']='test';
-		$config['custom']='DH';
+        $config['dbuser'] = $config['dbdatabase'] = 'dhtestbistro';
+        $config['barva'] = 'test';
+        $config['custom'] = 'DH';
 }
 
 
