f8aaaac2c92a3764db91cfa1b62f77e4b5f80c81
Karel Křemel <charles@alembiq.net>
2020-03-08 19:21:59 +0100

    db connection finetunning

--- inc/database.php
+++ inc/database.php
@@ -3,7 +3,7 @@
 $lines = file($config['dbpass'],FILE_IGNORE_NEW_LINES) or die("fail pwd");
 $password = $lines[2];
 
-$database = mysqli_connect ('localhost',$config['dbuser'],$password,$config['dbdatabase']) or die (mysqli_connect_errno()." ".mysqli_connect_error());
+$database = mysqli_connect ('localhost',$config['dbuser'],$password,$config['dbdatabase']) or die ($_SERVER["SERVER_NAME"].":".mysqli_connect_errno()." ".mysqli_connect_error());
 
 mysqli_query ($database,"SET NAMES 'utf8'");
 
