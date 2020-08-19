b2da339d6a1f07ae26c062848e5e5417570420e2
Karel Křemel <charles@alembiq.net>
2020-08-19 23:20:31 +0200

    case/task empty array

--- lib/task.php
+++ lib/task.php
@@ -9,6 +9,7 @@ function tasksAssignedTo($userid): array
 {
     global $database;
 
+    $unfinishedtasks[] = array();
 
     $tasksListSql = "SELECT * FROM ".DB_PREFIX."task WHERE ".DB_PREFIX."task.iduser=".$userid." AND ".DB_PREFIX."task.status=0 ORDER BY ".DB_PREFIX."task.created ASC";
     $tasksList = mysqli_query ($database,$tasksListSql);
