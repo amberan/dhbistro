5b21a91299b397b413fde1a0709c095f23655d15
Karel Křemel <charles@alembiq.net>
2020-12-15 00:14:06 +0100

    ECS upgrade

---
+++ .ecs.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use SlevomatCodingStandard\Sniffs\Commenting\DisallowCommentAfterCodeSniff;
+use SlevomatCodingStandard\Sniffs\Whitespaces\DuplicateSpacesSniff;
+use PhpCsFixer\Fixer\StringNotation\ExplicitStringVariableFixer;
+use PhpCsFixer\Fixer\Operator\IncrementStyleFixer;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use PhpCsFixer\Fixer\FunctionNotation\MethodArgumentSpaceFixer;
+use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
+use Symplify\CodingStandard\Fixer\Commenting\RemoveCommentedCodeFixer;
+use PhpCsFixer\Fixer\StringNotation\SimpleToComplexStringVariableFixer;
+use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+
+    $parameters = $containerConfigurator->parameters();
+//    $parameters->set('cache_directory', '.ecs_cache');
+    $parameters->set(Option::PATHS, [  __DIR__ . '.' ]);
+
+ $parameters->set(Option::SKIP,
+	[  __DIR__ . '/vendor/*' ,
+	ArrayOpenerAndCloserNewlineFixer::class,
+	ConcatSpaceFixer::class,
+        DeclareStrictTypesFixer::class,
+	DisallowCommentAfterCodeSniff::class,
+	DuplicateSpacesSniff::class,
+	ExplicitStringVariableFixer::class,
+	IncrementStyleFixer::class,
+	LineLengthFixer::class,
+	MethodArgumentSpaceFixer::class,
+	NotOperatorWithSuccessorSpaceFixer::class,
+	RemoveCommentedCodeFixer::class,
+	SimpleToComplexStringVariableFixer::class,
+	SingleQuoteFixer::class,
+	YodaStyleFixer::class,
+
+        //'Cognitive complexity for method "addAction" is 13 but has to be less than or equal to 8.',
+    ]);
+
+
+    $parameters->set(Option::SETS, [
+         SetList::PSR_1,
+         SetList::PSR_12,
+         SetList::PHP_70,
+         SetList::PHP_73_MIGRATION,
+         SetList::PHP_CS_FIXER,
+         SetList::PHP_71,
+         SetList::CLEAN_CODE,
+         SetList::SYMPLIFY,
+         SetList::ARRAY,
+         SetList::COMMON,
+//         SetList::COMMENTS,
+//         SetList::SPACES,
+         SetList::DEAD_CODE,
+         SetList::SYMFONY,
+    ]);
+
+    $parameters->set(Option::INDENTATION, 'spaces');
+    //$parameters->set(Option::LINE_ENDING, "\r\n");
+};
