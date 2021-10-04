f0a179d50292954a2ae9a9b48f190e06eb772e48
Karel Kremel <charles@alembiq.net>
2021-10-04 21:36:03 +0200

    Resolve "new symbols are not saving"

--- symbols.php
+++ symbols.php
@@ -2,28 +2,28 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'/inc/func_main.php';
 use Tracy\Debugger;
 
-Debugger::enable(Debugger::DETECT,$config['folder_logs']);
+Debugger::enable(Debugger::DETECT, $config['folder_logs']);
 
         // Přidání symbolu
     if (isset($_POST['insertsymbol'])) {
         if (is_uploaded_file($_FILES['symbol']['tmp_name'])) {
             $sfile = time().md5(uniqid(time().random_int(0, getrandmax())));
-            move_uploaded_file($_FILES['symbol']['tmp_name'],'./files/'.$sfile.'tmp');
-            $sdst = imageResize('./files/'.$sfile.'tmp',100,100);
-            imagejpeg($sdst,'./files/symbols/'.$sfile);
+            move_uploaded_file($_FILES['symbol']['tmp_name'], './files/'.$sfile.'tmp');
+            $sdst = imageResize('./files/'.$sfile.'tmp', 100, 100);
+            imagejpeg($sdst, './files/symbols/'.$sfile);
             unlink('./files/'.$sfile.'tmp');
         } else {
             $sfile = '';
         }
         $time = time();
-        $sql_p = "INSERT INTO ".DB_PREFIX."symbol (symbol, `desc`, deleted, created, created_by, modified, modified_by, archived, assigned, search_lines, search_curves, search_points, search_geometricals, search_alphabets, search_specialchars, secret)  VALUES( '".$sfile."', '".$_POST['contents']."', '0', '".$time."', '".$user['userId']."', '".$time."', '".$user['userId']."', 0, '0', '".$_POST['liner']."', '".$_POST['curver']."', '".$_POST['pointer']."', '".$_POST['geometrical']."', '".$_POST['alphabeter']."', '".$_POST['specialchar']."', 0)";
-        mysqli_query($database,$sql_p);
+        echo $sql_p = "INSERT INTO ".DB_PREFIX."symbol (symbol, `desc`, deleted, created, created_by, modified, modified_by, archived, assigned, search_lines, search_curves, search_points, search_geometricals, search_alphabets, search_specialchars, secret)  VALUES( '".$sfile."', '".$_POST['contents']."', '0', '".$time."', '".$user['userId']."', '".$time."', '".$user['userId']."', 0, '0', '".$_POST['liner']."', '".$_POST['curver']."', '".$_POST['pointer']."', '".$_POST['geometrical']."', '".$_POST['alphabeter']."', '".$_POST['specialchar']."', 0)";
+        mysqli_query($database, $sql_p);
         $sql_f = "SELECT id FROM ".DB_PREFIX."symbol WHERE created='".$time."' AND created_by='".$user['userId']."' AND modified='".$time."' AND modified_by='".$user['userId']."'";
-        $pidarray = mysqli_fetch_assoc(mysqli_query($database,$sql_f));
+        $pidarray = mysqli_fetch_assoc(mysqli_query($database, $sql_f));
         $pid = $pidarray['id'];
         auditTrail(7, 3, $pid);
         if (!isset($_POST['notnew'])) {
-            unreadRecords(7,$pid);
+            unreadRecords(7, $pid);
         }
         $_SESSION['message'] = 'Symbol vložen.';
     } elseif (isset($_POST['insertperson'])) {
@@ -32,14 +32,14 @@ Debugger::enable(Debugger::DETECT,$config['folder_logs']);
         // Vymazani symbolu
     if (isset($_REQUEST['sdelete']) && is_numeric($_REQUEST['sdelete']) && $usrinfo['right_text']) {
         auditTrail(7, 11, $_REQUEST['sdelete']);
-        mysqli_query($database,"UPDATE ".DB_PREFIX."symbol SET deleted=1 WHERE id=".$_REQUEST['sdelete']);
-        deleteAllUnread(7,$_REQUEST['sdelete']);
+        mysqli_query($database, "UPDATE ".DB_PREFIX."symbol SET deleted=1 WHERE id=".$_REQUEST['sdelete']);
+        deleteAllUnread(7, $_REQUEST['sdelete']);
         $_SESSION['message'] = 'Symbol smazan.';
     }
         // Obnoveni symbolu
     if (isset($_REQUEST['undelete']) && is_numeric($_REQUEST['undelete']) && $user['aclRoot']) {
         auditTrail(7, 11, $_REQUEST['undelete']);
-        mysqli_query($database,"UPDATE ".DB_PREFIX."symbol SET deleted=0 WHERE id=".$_REQUEST['undelete']);
+        mysqli_query($database, "UPDATE ".DB_PREFIX."symbol SET deleted=0 WHERE id=".$_REQUEST['undelete']);
         $_SESSION['message'] = 'Symbol obnoven.';
     }
 
@@ -47,26 +47,26 @@ Debugger::enable(Debugger::DETECT,$config['folder_logs']);
     if (isset($_POST['symbolid'], $_POST['editsymbol']) && $usrinfo['right_text']) {
         auditTrail(7, 2, $_POST['symbolid']);
         if (!isset($_POST['notnew'])) {
-            unreadRecords(7,$_POST['symbolid']);
+            unreadRecords(7, $_POST['symbolid']);
         }
         if (is_uploaded_file($_FILES['symbol']['tmp_name'])) {
-            $sps = mysqli_query($database,"SELECT symbol FROM ".DB_PREFIX."symbol WHERE id=".$_POST['symbolid']);
+            $sps = mysqli_query($database, "SELECT symbol FROM ".DB_PREFIX."symbol WHERE id=".$_POST['symbolid']);
             if ($spc = mysqli_fetch_assoc($sps)) {
                 unlink('./files/symbols/'.$spc['symbol']);
             }
             $sfile = time().md5(uniqid(time().random_int(0, getrandmax())));
-            move_uploaded_file($_FILES['symbol']['tmp_name'],'./files/'.$sfile.'tmp');
-            $sdst = imageResize('./files/'.$sfile.'tmp',100,100);
-            imagejpeg($sdst,'./files/symbols/'.$sfile);
+            move_uploaded_file($_FILES['symbol']['tmp_name'], './files/'.$sfile.'tmp');
+            $sdst = imageResize('./files/'.$sfile.'tmp', 100, 100);
+            imagejpeg($sdst, './files/symbols/'.$sfile);
             unlink('./files/'.$sfile.'tmp');
-            mysqli_query($database,"UPDATE ".DB_PREFIX."symbol SET symbol='".$sfile."' WHERE id=".$_POST['symbolid']);
+            mysqli_query($database, "UPDATE ".DB_PREFIX."symbol SET symbol='".$sfile."' WHERE id=".$_POST['symbolid']);
         }
         if ($user['aclGamemaster'] == 1) {
             $sql = "UPDATE ".DB_PREFIX."symbol SET `desc`='".$_POST['desc']."', search_lines='".$_POST['liner']."', search_curves='".$_POST['curver']."', search_points='".$_POST['pointer']."', search_geometricals='".$_POST['geometrical']."', search_alphabets='".$_POST['alphabeter']."', search_specialchars='".$_POST['specialchar']."' WHERE id=".$_POST['symbolid'];
-            mysqli_query($database,$sql);
+            mysqli_query($database, $sql);
         } else {
             $sql = "UPDATE ".DB_PREFIX."symbol SET `desc`='".$_POST['desc']."', modified='".time()."', modified_by='".$user['userId']."', search_lines='".$_POST['liner']."', search_curves='".$_POST['curver']."', search_points='".$_POST['pointer']."', search_geometricals='".$_POST['geometrical']."', search_alphabets='".$_POST['alphabeter']."', search_specialchars='".$_POST['specialchar']."' WHERE id=".$_POST['symbolid'];
-            mysqli_query($database,$sql);
+            mysqli_query($database, $sql);
         }
         $_SESSION['message'] = 'Symbol upraven.';
     } elseif (isset($_POST['editsymbol'])) {
@@ -75,13 +75,13 @@ Debugger::enable(Debugger::DETECT,$config['folder_logs']);
       // archivace symbolu
     if (isset($_GET['archive']) && is_numeric($_GET['archive']) && $usrinfo['right_text']) {
         auditTrail(7, 11, $_GET['archive']);
-        mysqli_query($database,'UPDATE '.DB_PREFIX.'symbol SET archived=CURRENT_TIMESTAMP WHERE id='.$_GET['archive']);
+        mysqli_query($database, 'UPDATE '.DB_PREFIX.'symbol SET archived=CURRENT_TIMESTAMP WHERE id='.$_GET['archive']);
         $_SESSION['message'] = 'Symbol archivovan.';
     }
       // odarchivace symbolu
     if (isset($_GET['unarchive']) && is_numeric($_GET['unarchive']) && $usrinfo['right_text']) {
         auditTrail(7, 11, $_GET['unarchive']);
-        mysqli_query($database,'UPDATE '.DB_PREFIX.'symbol SET archived=0 WHERE id='.$_GET['unarchive']);
+        mysqli_query($database, 'UPDATE '.DB_PREFIX.'symbol SET archived=0 WHERE id='.$_GET['unarchive']);
         $_SESSION['message'] = 'Symbol odarchivovan.';
     }
 
@@ -90,18 +90,18 @@ latteDrawTemplate("header");
 $latteParameters['title'] = 'Symboly';
     auditTrail(7, 1, 0);
     mainMenu();
-    deleteUnread(7,'none');
-    sparklets('<a href="persons.php">osoby</a> &raquo; <strong>nepřiřazené symboly</strong>','<a href="newsymbol.php">nový symbol</a>; <a href="symbol_search.php">vyhledat symbol</a>');
+    deleteUnread(7, 'none');
+    sparklets('<a href="persons.php">osoby</a> &raquo; <strong>nepřiřazené symboly</strong>', '<a href="newsymbol.php">nový symbol</a>; <a href="symbol_search.php">vyhledat symbol</a>');
 
-if (sizeof($_POST['filter']) > 0) {
-    filterSet('symbol',@$_POST['filter']);
-}
+    if (sizeof($_POST['filter']) > 0) {
+        filterSet('symbol', @$_POST['filter']);
+    }
 $filter = filterGet('symbol');
 $sqlFilter = DB_PREFIX.'symbol.deleted in (0,'.$user['aclRoot'].') '; // AND ".DB_PREFIX."case.secret<=".$user['aclSecret'];
 
 switch (@$filter['archived']) {
-   case 'on': $sqlFilter .= ' AND '.DB_PREFIX.'symbol.archived is not null '; break;
-   default: $sqlFilter .= ' AND '.DB_PREFIX.'symbol.archived is null ';
+   case 'on': $sqlFilter .= ' AND '.DB_PREFIX.'symbol.archived >= 0 '; break;
+   default: $sqlFilter .= ' AND '.DB_PREFIX.'symbol.archived = 0 ';
 }
 switch (@$filter['deleted']) {
    case 'on': $sqlFilter .= ' AND '.DB_PREFIX.'symbol.deleted>=0 '; break;
@@ -111,7 +111,7 @@ switch (@$filter['deleted']) {
 ?>
 
 <form action="symbols.php" method="POST" id="filter">
-<input type="hidden" name="placeholder"  />
+<input type="hidden" name="filter[placeholder]"  />
 <input type="checkbox" name="filter[archived]" <?php if (isset($filter['archived']) and $filter['archived'] == 'on') { ?> checked <?php } ?> onchange="this.form.submit()"/>i archivovane
 <?php if ($user['aclRoot'] > 0) { ?> <input type="checkbox" name="filter[deleted]" <?php if (isset($filter['deleted']) and $filter['deleted'] == 'on') { ?> checked <?php } ?> onchange="this.form.submit()"/>i smazane <?php } ?>
 </form>
@@ -120,7 +120,7 @@ switch (@$filter['deleted']) {
     $sql = "SELECT * FROM ".DB_PREFIX."symbol
         WHERE ".$sqlFilter." AND ".DB_PREFIX."symbol.assigned=0
         ORDER BY ".DB_PREFIX."symbol.created DESC";
-    $res = mysqli_query($database,$sql);
+    $res = mysqli_query($database, $sql);
     if (mysqli_num_rows($res)) {
         echo '<div id="obsah">
 <table>
@@ -134,7 +134,7 @@ switch (@$filter['deleted']) {
 <tbody>';
         $even = 0;
         while ($rec = mysqli_fetch_assoc($res)) {
-            echo '<tr class="'.(searchRecord(7,$rec['id']) ? ' unread_record' : ($even % 2 == 0 ? 'even' : 'odd')).'">
+            echo '<tr class="'.(searchRecord(7, $rec['id']) ? ' unread_record' : ($even % 2 == 0 ? 'even' : 'odd')).'">
 		  <td><a href="readsymbol.php?rid='.$rec['id'].'"><img src="file/symbol/'.$rec['id'].'" alt="symbol chybí" /></a></td>
 		  <td>'.stripslashes($rec['desc']).'<br />';
             // generování poznámek
@@ -144,7 +144,7 @@ switch (@$filter['deleted']) {
             } else {
                 $sql_n = "SELECT ".DB_PREFIX."note.iduser AS 'iduser', ".DB_PREFIX."note.title AS 'title', ".DB_PREFIX."note.note AS 'note', ".DB_PREFIX."note.secret AS 'secret', ".DB_PREFIX."user.userName AS 'user', ".DB_PREFIX."note.id AS 'id' FROM ".DB_PREFIX."note, ".DB_PREFIX."user WHERE ".DB_PREFIX."note.iduser=".DB_PREFIX."user.userId AND ".DB_PREFIX."note.iditem=".$rec['id']." AND ".DB_PREFIX."note.idtable=7 AND ".DB_PREFIX."note.deleted=0 AND (".DB_PREFIX."note.secret=0 OR ".DB_PREFIX."note.iduser=".$user['userId'].") ORDER BY ".DB_PREFIX."note.datum DESC";
             }
-            $res_n = mysqli_query($database,$sql_n);
+            $res_n = mysqli_query($database, $sql_n);
             $i = 0;
             while ($rec_n = mysqli_fetch_assoc($res_n)) {
                 $i++;
@@ -184,7 +184,7 @@ switch (@$filter['deleted']) {
             } else {
                 $sql_s = "SELECT ".DB_PREFIX."case.id AS 'id', ".DB_PREFIX."case.title AS 'title' FROM ".DB_PREFIX."symbol2all, ".DB_PREFIX."case WHERE ".DB_PREFIX."case.id=".DB_PREFIX."symbol2all.idrecord AND ".DB_PREFIX."symbol2all.idsymbol=".$rec['id']." AND ".DB_PREFIX."symbol2all.table=3 AND ".DB_PREFIX."case.secret=0 ORDER BY ".DB_PREFIX."case.title ASC";
             }
-            $pers = mysqli_query($database,$sql_s);
+            $pers = mysqli_query($database, $sql_s);
 
             $i = 0;
             while ($perc = mysqli_fetch_assoc($pers)) {
@@ -210,7 +210,7 @@ switch (@$filter['deleted']) {
             } else {
                 $sql_s = "SELECT ".DB_PREFIX."report.id AS 'id', ".DB_PREFIX."report.label AS 'label' FROM ".DB_PREFIX."symbol2all, ".DB_PREFIX."report WHERE ".DB_PREFIX."report.id=".DB_PREFIX."symbol2all.idrecord AND ".DB_PREFIX."symbol2all.idsymbol=".$rec['id']." AND ".DB_PREFIX."symbol2all.table=4 AND ".DB_PREFIX."report.secret=0 ORDER BY ".DB_PREFIX."report.label ASC";
             }
-            $pers = mysqli_query($database,$sql_s);
+            $pers = mysqli_query($database, $sql_s);
 
             $i = 0;
             while ($perc = mysqli_fetch_assoc($pers)) {
