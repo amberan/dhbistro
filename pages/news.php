f7a2b05ebdffc9d50dadc8698e9a9583cbaf62d7
Karel Kremel <charles@alembiq.net>
2021-10-04 09:36:34 +0200

    bugfix: can't save new `news` #156

--- pages/news.php
+++ pages/news.php
@@ -2,10 +2,10 @@
 
 use Tracy\Debugger;
 
-Debugger::enable(Debugger::DETECT,$config['folder_logs']);
+Debugger::enable(Debugger::DETECT, $config['folder_logs']);
 
 if (isset($URL['3']) and $URL['1'] == "news" and ($user['aclDeputy'] > 0 or $user['aclDirector']) and $URL['2'] == "delete") { // DELETE
-    mysqli_query($database,"UPDATE ".DB_PREFIX."news set deleted=1 where id='".$URL['3']."'");
+    mysqli_query($database, "UPDATE ".DB_PREFIX."news set deleted=1 where id='".$URL['3']."'");
     if (mysqli_affected_rows($database) == 1) {
         auditTrail(5, 11, $URL['3']);
         $latteParameters['message'] = $text['aktualitaodebrana'];
@@ -18,7 +18,7 @@ if (isset($URL['3']) and $URL['1'] == "news" and ($user['aclDeputy'] > 0 or $use
 }
 
 if (isset($URL['3']) and $URL['1'] == "news" and ($user['aclDeputy'] > 0 or $user['aclDirector']) and $URL['2'] == "restore") { // DELETE
-    mysqli_query($database,"UPDATE ".DB_PREFIX."news set deleted=0 where id='".$URL['3']."'");
+    mysqli_query($database, "UPDATE ".DB_PREFIX."news set deleted=0 where id='".$URL['3']."'");
     if (mysqli_affected_rows($database) == 1) {
         auditTrail(5, 11, $URL['3']);
         $latteParameters['message'] = $text['aktualitaobnovena'];
@@ -31,12 +31,12 @@ if (isset($URL['3']) and $URL['1'] == "news" and ($user['aclDeputy'] > 0 or $use
 }
 
 if ($URL['1'] == "news" and ($user['aclDeputy'] > 0 or $user['aclDirector']) and isset($_POST['news_new'])) { // ADD
-    if ($_POST['insertnews'] && !preg_match('/^[[:blank:]]*$/i',$_POST['nadpis']) && !preg_match('/^[[:blank:]]*$/i',$_POST['news_new']) && is_numeric($_POST['kategorie'])) {
-        mysqli_query($database,"INSERT INTO ".DB_PREFIX."news ( datum, iduser, kategorie, nadpis, obsah, obsahMD, deleted) VALUES('".time()."','".$user['userId']."','".$_POST['kategorie']."','".$_POST['nadpis']."','','".$_POST['news_new']."',0)");
+    if ($_POST['insertnews'] && !preg_match('/^[[:blank:]]*$/i', $_POST['nadpis']) && !preg_match('/^[[:blank:]]*$/i', $_POST['news_new']) && is_numeric($_POST['kategorie'])) {
+        mysqli_query($database, "INSERT INTO ".DB_PREFIX."news ( datum, iduser, kategorie, nadpis, obsahMD, deleted) VALUES('".time()."','".$user['userId']."','".$_POST['kategorie']."','".$_POST['nadpis']."','".$_POST['news_new']."',0)");
         if (mysqli_affected_rows($database) == 1) {
             auditTrail(5, 3, 0);
             $latteParameters['message'] = $text['aktualitavlozena'];
-            unreadRecords(5,0);
+            unreadRecords(5, 0);
         } else {
             $latteParameters['message'] = $text['aktualitanevlozena'];
         }
@@ -45,11 +45,11 @@ if ($URL['1'] == "news" and ($user['aclDeputy'] > 0 or $user['aclDirector']) and
     }
 }
 
-deleteUnread(5,0);
+deleteUnread(5, 0);
 $sql_news = "SELECT ".DB_PREFIX."news.* , ".DB_PREFIX."user.userName AS 'author'
 FROM ".DB_PREFIX."news JOIN ".DB_PREFIX."user ON ".DB_PREFIX."news.iduser = ".DB_PREFIX."user.userId
 WHERE ".$user['sqlDeleted']." ORDER BY ".DB_PREFIX."news.datum DESC LIMIT 10";
-$news_query = mysqli_query($database,$sql_news);
+$news_query = mysqli_query($database, $sql_news);
 if (mysqli_num_rows($news_query)) {
     while ($news_record = mysqli_fetch_assoc($news_query)) {
         if (isset($news_record['obsahMD']) && strlen($news_record['obsahMD']) > 0) {
