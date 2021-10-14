436794d6c23360fac032bb031c6d0dfc1c4e87d7
Karel Křemel <charles@alembiq.net>
2021-10-14 17:59:03 +0200

    feature:  #151

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,58 +1,58 @@
-<?php return array (
-  'root' => 
-  array (
+<?php return array(
+  'root' =>
+  array(
     'pretty_version' => 'dev-master',
     'version' => 'dev-master',
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
     ),
     'reference' => 'f90ec9d7ad214187943593f2ce3033f78d3819ef',
     'name' => '__root__',
   ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
+  'versions' =>
+  array(
+    '__root__' =>
+    array(
       'pretty_version' => 'dev-master',
       'version' => 'dev-master',
-      'aliases' => 
-      array (
+      'aliases' =>
+      array(
       ),
       'reference' => 'f90ec9d7ad214187943593f2ce3033f78d3819ef',
     ),
-    'latte/latte' => 
-    array (
+    'latte/latte' =>
+    array(
       'pretty_version' => 'v2.10.4',
       'version' => '2.10.4.0',
-      'aliases' => 
-      array (
+      'aliases' =>
+      array(
       ),
       'reference' => '6e0540c60805b33148dc95db2602a087fb2ef9fb',
     ),
-    'league/html-to-markdown' => 
-    array (
+    'league/html-to-markdown' =>
+    array(
       'pretty_version' => '4.10.0',
       'version' => '4.10.0.0',
-      'aliases' => 
-      array (
+      'aliases' =>
+      array(
       ),
       'reference' => '0868ae7a552e809e5cd8f93ba022071640408e88',
     ),
-    'symplify/easy-coding-standard' => 
-    array (
+    'symplify/easy-coding-standard' =>
+    array(
       'pretty_version' => '9.4.70',
       'version' => '9.4.70.0',
-      'aliases' => 
-      array (
+      'aliases' =>
+      array(
       ),
       'reference' => '9276b0c7a1a5671fc4a067656ab40d0aea42aaca',
     ),
-    'tracy/tracy' => 
-    array (
+    'tracy/tracy' =>
+    array(
       'pretty_version' => 'v2.8.7',
       'version' => '2.8.7.0',
-      'aliases' => 
-      array (
+      'aliases' =>
+      array(
       ),
       'reference' => '8e708de7c611f626c8792d43f1c78812ea24e6f6',
     ),
